36d2b31cfbb5105d45254639a9ab74e80967989b
Dom Henderson <dominic.henderson1@outlook.com>
2021-09-19 10:37:18 +0100

    Created bot

---
+++ src/UpdateBotList.ts
@@ -0,0 +1,56 @@
+import axios from 'axios';
+import fs from 'fs';
+
+export async function UpdateBotList() {
+	const channelMinimum = 20;
+	const daysMinimum = 30;
+	const lastOnline = Math.floor(Date.now() / 1000) - daysMinimum * 24 * 60 * 60;
+	const whitelist = JSON.parse(fs.readFileSync('./whitelist.json', 'utf8'));
+	const bots = await fetchList();
+	filterList(channelMinimum, daysMinimum, lastOnline, whitelist, bots);
+}
+
+async function fetchList() {
+	console.log('Fetching botlist, this will take a moment..');
+	const res: any = await axios.get('https://api.twitchinsights.net/v1/bots/all');
+	const headerDate = res.headers && res.headers.Date ? res.headers.Date : 'no response date';
+	console.log('Status code:', res.statusCode);
+	console.log('Date in Response header', headerDate);
+
+	const json = res.data;
+	console.log('Received list of ' + json.bots.length + ' names.');
+	return json.bots;
+}
+
+function filterList(channelMinimum: number, daysMinimum: number, lastOnline: number, whitelist: string[], bots: any) {
+	console.log('Filtering list..');
+	console.log('Minimum Channels the bot needs to be in: ' + channelMinimum);
+	console.log('Last Online within: ' + daysMinimum + ' days');
+	let filteredBots = [];
+
+	for (let i = 0; i < bots.length; i++) {
+		let bot = bots[i];
+		let name = bot[0];
+
+		// check channel count
+		if (bot[1] < channelMinimum) {
+		continue;
+		}
+
+		// check last activity
+		if (bot[2] < lastOnline) {
+		continue;
+		}
+
+		// check whitelist
+		if (whitelist.includes(name)) {
+			continue;
+		}
+
+		filteredBots.push(name);
+	}
+
+	filteredBots.sort();
+	fs.writeFileSync('list.json', JSON.stringify(filteredBots, null, 2), 'utf8');
+	console.log('The list.json now contains ' + filteredBots.length + ' usernames.');
+}
---
+++ src/index.ts
@@ -0,0 +1,69 @@
+import axios from 'axios';
+import fs from 'fs';
+import tmi from 'tmi.js';
+import { UpdateBotList } from './UpdateBotList';
+import { ClientOptions } from './secret/secrets';
+
+const client: tmi.Client = new tmi.client(ClientOptions);
+
+// Register our event handlers (defined below)
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+client.on('join', onJoinHandler);
+
+// Connect to Twitch:
+client.connect();
+
+UpdateBotList();
+
+// Called every time a message comes in
+function onMessageHandler (target: string, context: tmi.ChatUserstate, msg: string, self: boolean) {
+	if (self) { return; } // Ignore messages from the bot
+
+	// Remove whitespace from chat message
+	const splitMessage: string[] = msg.trim().split(" ");
+	if(!splitMessage.length) { return; }
+	console.log(splitMessage);
+	const commandName: string = splitMessage[0].slice(1);
+	const args: string[] = splitMessage.slice(1);
+
+	if(splitMessage[0][0] !== '!') { return; } // Ignore messages that aren't commands
+
+	// If the command is known, let's execute it
+	if (
+		commandName.toLocaleLowerCase() === 'istrustedbot' ||
+		commandName.toLocaleLowerCase() === 'itb'
+	) {
+		if(isUntrustedBot(args[0])) {
+			client.say(target, `/me ${args[0]} is an untrusted bot`);
+		} else {
+			client.say(target, `/me ${args[0]} is not an untrusted bot`);
+		}
+	} else {
+		console.log(`* Unknown command ${commandName}`);
+	}
+}
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler (addr: string, port: number) {
+	console.log(`* Connected to ${addr}:${port}`);
+}
+
+function isUntrustedBot(username: string): boolean {
+	const botList: string[] = JSON.parse(fs.readFileSync('./list.json', 'utf8'));
+	return botList.includes(username);
+}
+
+function onJoinHandler(channel: string, username: string, self: boolean) {
+	console.log(channel);
+	console.log(username);
+	console.log(self);
+	if(isUntrustedBot(username)) {
+		console.log(`${username} is an untrusted bot, banning`);
+		client.say(channel, `/ban ${username}`);
+	} else {
+		console.log(`${username} is not an untrusted bot`);
+	}
+}
+
+setInterval(UpdateBotList, 6*60*1000);
