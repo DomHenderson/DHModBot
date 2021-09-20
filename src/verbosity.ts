13b6b13dc72ca8f65416e8dd3d6bf3bbc481c8c1
Dom Henderson <dominic.henderson1@outlook.com>
2021-09-20 11:44:52 +0100

    Fixed verbosity file io

--- src/verbosity.ts
+++ src/verbosity.ts
@@ -1,8 +1,11 @@
 import fs from 'fs';
 
-let verbosityLevels: Map<string, string> = JSON.parse(fs.readFileSync('./src/secret/verbosity.json', 'utf-8'));
+let verbosityLevels: Map<string, string> = new Map<string, string>();
+
+loadLevels();
 
 export function getQuiet(channel: string): boolean {
+	console.log(`Checking quietness for ${channel}`);
 	return verbosityLevels.get(channel) == 'quiet';
 }
 
@@ -16,5 +19,21 @@ export function setQuiet(channel: string): void {
 
 function setVerbosity(channel: string, level: string): void {
 	verbosityLevels.set(channel, level);
-	fs.writeFileSync('verbosity.json', JSON.stringify(verbosityLevels, null, 4), 'utf8');
+	saveLevels();
+}
+
+function loadLevels() {
+	verbosityLevels.clear();
+	const json = JSON.parse(fs.readFileSync('./src/secret/verbosity.json', 'utf-8'));
+	for(let value in json) {
+		verbosityLevels.set(value, json[value]);
+	}
+}
+
+function saveLevels() {
+	let json: any = {};
+	verbosityLevels.forEach((value, key) => {
+		json[key] = value;
+	})
+	fs.writeFileSync('./src/secret/verbosity.json', JSON.stringify(json, null, 4), 'utf8');
 }
